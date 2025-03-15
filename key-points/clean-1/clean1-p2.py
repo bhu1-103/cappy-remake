98cf7d692cf0f57f0bca049ee4832e421ba2be49
bhu1-103 <103.bhuvan@gmail.com>
2025-03-15 20:28:04 +0530

    a few points to note

---
+++ key-points/clean-1/clean1-p2.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+file_path = "../../test/step2/z_output/input_nodes_copy_deployment_000.csv"
+
+df = pd.read_csv(file_path, delimiter=";")
+df_numeric = df.drop(df.columns[[0, 2]], axis=1)
+variance = df_numeric.var()
+print(variance)
+
+df_cleaned = df_numeric.drop(columns=variance[variance == 0].index)
+
+df_cleaned.to_csv("clean1-part2.csv",index=False,sep=";")
